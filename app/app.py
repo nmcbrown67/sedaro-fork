6c68ac01814c02eeb7826ad86aa35f2168916125
Mateen Kasim <mateen.kasim@sedarotech.com>
2024-10-24 13:04:17 -0400

    Simple sqlite db

--- app/app.py
+++ app/app.py
@@ -1,15 +1,42 @@
 # HTTP SERVER
 
 import json
-import os
 
 from flask import Flask, request
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 from simulator import Simulator
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from store import QRangeStore
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+############################## Application Configuration ##############################
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:5000"])
+
+db = SQLAlchemy(model_class=Base)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db.init_app(app)
+
+
+############################## Database Models ##############################
+
+
+class Simulation(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    data: Mapped[str]
+
+
+with app.app_context():
+    db.create_all()
+
+
+############################## API Endpoints ##############################
 
 
 @app.get("/")
@@ -19,17 +46,9 @@ def health():
 
 @app.get("/simulation")
 def get_data():
-
-    # If data.json doesn't exist, return empty data
-    if not os.path.exists("./data.json"):
-        data = []
-
-    # Else return json from data.json
-    else:
-        with open("./data.json") as f:
-            data = json.load(f)
-
-    return data
+    # Get most recent simulation from database
+    simulation: Simulation = Simulation.query.order_by(Simulation.id.desc()).first()
+    return simulation.data if simulation else []
 
 
 @app.post("/simulation")
@@ -40,7 +59,7 @@ def simulate():
     #     "Satellite": {"x": 0, "y": 1, "vx": 1, "vy": 0},
     # }
 
-    # Define time and timeStep for each object
+    # Define time and timeStep for each agent
     init: dict = request.json
     for key in init.keys():
         init[key]["time"] = 0
@@ -53,8 +72,9 @@ def simulate():
     # Run simulation
     simulator.simulate()
 
-    # Save data to data.json
-    with open("./data.json", "w") as f:
-        f.write(json.dumps(store.store, indent=4))
+    # Save data to database
+    simulation = Simulation(data=json.dumps(store.store))
+    db.session.add(simulation)
+    db.session.commit()
 
     return store.store
